5b21b7a85355b86cf62b1813fdcad5fcb2525791
Leandro <leandro.klingeroliveira@gmail.com>
2021-04-02 13:38:17 -0300

    Update and Remove Category

--- Ragnarok/Areas/Employee/Controllers/CategoryController.cs
+++ Ragnarok/Areas/Employee/Controllers/CategoryController.cs
@@ -47,5 +47,38 @@ namespace Ragnarok.Areas.Employee.Controllers
             }
             return View(category);
         }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Category category = _categoryRepository.FindById(id, _employeeLogin.GetEmployee().BusinessId);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Update(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.UpdateDate = DateTime.Now;
+                _categoryRepository.Update(category);
+                TempData["MSG_S"] = Message.MSG_S_001;
+                return RedirectToAction(nameof(Details), new { id = category.Id });
+            }
+            return View(nameof(Details), category);
+        }
+        [HttpGet]
+        public IActionResult Remove(int id)
+        {
+            try
+            {
+                _categoryRepository.Remove(id, _employeeLogin.GetEmployee().BusinessId);
+                TempData["MSG_S"] = Message.MSG_S_005;
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                TempData["MSG_E"] = Message.MSG_E_003;
+                return RedirectToAction(nameof(Index));
+            }
+        }
     }
 }
